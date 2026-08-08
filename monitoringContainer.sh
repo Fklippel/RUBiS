dfc9839c48b7d6f2bdc77a3a1db90c916d5ce9fc
Klippel21 <laura.klippel@edu.pucrs.br>
2026-08-08 20:01:25 -0300

    monitoring container stats

---
+++ monitoringContainer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Monitora CPU, memória, rede e disco dos containers web e db do RUBiS
+# Uso: ./monitor_containers.sh > container_stats.csv
+
+echo "timestamp,container,cpu_perc,mem_usage,mem_perc,net_io,block_io"
+
+while true; do
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  docker stats --no-stream --format "{{.Name}},{{.CPUPerc}},{{.MemUsage}},{{.MemPerc}},{{.NetIO}},{{.BlockIO}}" rubis-web-1 rubis-db-1 | \
+  while IFS=',' read -r name cpu mem_usage mem_perc net_io block_io; do
+    echo "$timestamp,$name,$cpu,$mem_usage,$mem_perc,$net_io,$block_io"
+  done
+  sleep 2
+done
