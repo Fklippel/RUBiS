2df4612ced4e44934fb9ba57db9ca3fc56ff0f82
Klippel21 <laura.klippel@edu.pucrs.br>
2026-08-08 13:59:48 -0300

    removing servlets and ejb lines

--- bench/generate_cpu_avg.sh
+++ bench/generate_cpu_avg.sh
@@ -7,14 +7,10 @@ for i in 200* ; do
     echo $i/
     gunzip $i"/db_server.gz"
     gunzip $i"/web_server.gz"
-    gunzip $i"/servlets_server.gz"
-    gunzip $i"/ejb_server.gz"
     format_cpu_output.awk $i"/db_server"
     format_cpu_output.awk $i"/web_server"
     gzip -9 $i"/db_server" &
     gzip -9 $i"/web_server" &
-    gzip -9 $i"/servlets_server" &
-    gzip -9 $i"/ejb_server" &
 
     grep "set" $i"/index.html"
     grep "Total number of clients" $i"/index.html"
@@ -22,10 +18,6 @@ for i in 200* ; do
     cpu_avg.awk $i"/db_server.cpu.busy.dat"
     echo -n "WS :"
     cpu_avg.awk $i"/web_server.cpu.busy.dat"
-    echo -n "Servlet :"
-    cpu_avg.awk $i"/servlets_server.cpu.busy.dat"
-    echo -n "EJB :"
-    cpu_avg.awk $i"/ejb_server.cpu.busy.dat"    
     rm -f $i/*.dat
     echo "."
   fi
